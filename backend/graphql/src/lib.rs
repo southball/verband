a376be432076bc5bcc677c6fd2d90838dfda5a49
Southball <6523469+southball@users.noreply.github.com>
2024-01-02 04:19:51 +0000

    Setup sessions

--- backend/graphql/src/lib.rs
+++ backend/graphql/src/lib.rs
@@ -1,4 +1,5 @@
-use async_graphql::{EmptyMutation, EmptySubscription, Object};
+use async_graphql::{Context, EmptyMutation, EmptySubscription, Object};
+use tower_sessions::Session;
 
 pub struct QueryRoot;
 
@@ -7,10 +8,31 @@ impl QueryRoot {
     async fn hello(&self) -> String {
         "Hello, world!".to_string()
     }
+
+    async fn logged_in(&self, ctx: &Context<'_>) -> async_graphql::Result<bool> {
+        let session = ctx.data::<Session>()?;
+        Ok(session.get::<String>("logged_in")? == Some("1".to_string()))
+    }
 }
 
-pub type VerbandSchema = async_graphql::Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+pub struct MutationRoot;
+
+#[Object]
+impl MutationRoot {
+    async fn login(
+        &self,
+        ctx: &Context<'_>,
+        username: String,
+        password: String,
+    ) -> async_graphql::Result<bool> {
+        let session = ctx.data::<Session>()?;
+        session.insert("logged_in", "1".to_string())?;
+        Ok(true)
+    }
+}
+
+pub type VerbandSchema = async_graphql::Schema<QueryRoot, MutationRoot, EmptySubscription>;
 
 pub fn verband_schema() -> VerbandSchema {
-    VerbandSchema::build(QueryRoot, EmptyMutation, EmptySubscription).finish()
+    VerbandSchema::build(QueryRoot, MutationRoot, EmptySubscription).finish()
 }
--- backend/server/src/main.rs
+++ backend/server/src/main.rs
@@ -1,12 +1,18 @@
 use async_graphql::http::GraphiQLSource;
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
 use axum::{
-    extract::State,
+    error_handling::HandleErrorLayer,
+    http::StatusCode,
     response::{Html, IntoResponse},
     routing::get,
-    Router, Server,
+    BoxError, Extension, Router, Server,
 };
 use std::net::SocketAddr;
+use tower::ServiceBuilder;
+use tower_sessions::{
+    cookie::time::Duration, sqlx::postgres::PgPoolOptions, Expiry, PostgresStore, Session,
+    SessionManagerLayer,
+};
 use verband_graphql::{verband_schema, VerbandSchema};
 
 async fn graphiql() -> impl IntoResponse {
@@ -14,19 +20,38 @@ async fn graphiql() -> impl IntoResponse {
 }
 
 async fn graphql_handler(
-    State(schema): State<VerbandSchema>,
+    Extension(schema): Extension<VerbandSchema>,
+    session: Session,
     req: GraphQLRequest,
 ) -> GraphQLResponse {
-    schema.execute(req.into_inner()).await.into()
+    schema.execute(req.into_inner().data(session)).await.into()
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let database_url = std::env::var("DATABASE_URL")?;
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+    let session_store = PostgresStore::new(pool.clone());
+    session_store.migrate().await?;
+    let session_layer = ServiceBuilder::new()
+        .layer(HandleErrorLayer::new(|_: BoxError| async {
+            StatusCode::BAD_REQUEST
+        }))
+        .layer(
+            SessionManagerLayer::new(session_store)
+                .with_secure(false)
+                .with_expiry(Expiry::OnInactivity(Duration::weeks(1))),
+        );
+
     let schema = verband_schema();
 
     let app = Router::new()
         .route("/api/graphql", get(graphiql).post(graphql_handler))
-        .with_state(schema);
+        .layer(Extension(schema))
+        .layer(session_layer);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
 
