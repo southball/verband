98610c68d0169270f7ebbe1cc4cd6dcfe66ad004
Southball <6523469+southball@users.noreply.github.com>
2024-01-05 05:10:55 +0000

    Add Monaco editor

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,18 @@
+import Editor from '@monaco-editor/react';
+import { editor as monacoEditor } from 'monaco-editor';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [editor, setEditor] = useState<monacoEditor.IStandaloneCodeEditor | null>(null);
+  useEffect(() => {
+    editor?.updateOptions({
+      mouseWheelZoom: true
+    })
+  }, [editor])
+
   return (
     <>
-      <div>Hello, world!</div>
+      <Editor height="100vh" language="markdown" onMount={(editor, _monaco) => setEditor(editor)} />
     </>
   )
 }
